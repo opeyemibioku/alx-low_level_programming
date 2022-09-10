e6bb91403a85e31db1df56f711068dd12508c41b
opeyemibioku <opeyemibioku@gmail.com>
2022-09-09 17:30:29 -0700

    Variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,44 @@
+Task ### ### #.txt
+Who has access
+
+System properties
+Type
+Text
+Size
+388 bytes
+Storage used
+388 bytes
+Location
+0x01-variables_if_else_while
+Owner
+Antony Bahati
+Modified
+Sep 9, 2022 by Antony Bahati
+Opened
+1:16 AM by me
+Created
+Sep 9, 2022
+No description
+Viewers can download
+#include <stdio.h>
+/**
+ * main - print if the number is postive, zero, or negative
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
